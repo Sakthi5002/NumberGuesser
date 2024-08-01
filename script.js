167fc26455a41e6539c910fec8ae5dd38e670e8a
Sakthi5002 <sakthiselvamani2005@gmail.com>
2024-08-01 21:07:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const okButton = document.getElementById("ok-btn");
 const score = document.getElementById("score");
 const resetButton = document.getElementById("reset-btn");
 const resultText = document.getElementById("result");
+const tryText = document.getElementById("try");
 var randomNumber = randomGenerator();
 var scoreValue = 0;
+var tryValue = 0;
 
 function randomGenerator() {
     var number = Math.round(Math.random() * 5);
@@ -16,8 +18,14 @@ var click = () => {
         scoreValue++;
         score.innerText = `Score: ${scoreValue}`;
         resultText.innerText = "Correct!";
+        tryValue = 0;
+        setTimeout(() => {
+            tryText.innerText = `Tries: 0`;
+        }, 1500);
     }
     else {
+        tryValue++;
+        tryText.innerText = `Tries: ${tryValue}`;
         resultText.innerText = "Oops. Try Again!";
     }
     setTimeout(() => {
@@ -32,6 +40,7 @@ var reset = () => {
     randomNumber = randomGenerator();
     resultText.innerText = "‎ ";
     input.value = "";
+    tryValue = 0;
+    tryText.innerText = `Tries: 0`;
 }
 resetButton.onclick = reset;
-
